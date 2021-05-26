85e79d66c22bd26496da1580ed7c68c3185446f1
Vicki Mes <kemboivictor3@gmail.com>
2021-05-26 16:13:58 +0300

    comments clearing.

--- app/src/main/java/com/mes/shiestywave/ui/view/Container.kt
+++ app/src/main/java/com/mes/shiestywave/ui/view/Container.kt
@@ -27,12 +27,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            ShiestyWaveTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(color = MaterialTheme.colors.background) {
-//                    Greeting("Android")
-//                }
-//            }
             ShiestyWaveApp()
         }
     }
